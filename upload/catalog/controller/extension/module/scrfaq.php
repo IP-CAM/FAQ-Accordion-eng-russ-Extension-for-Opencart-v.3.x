1274f30f16345686ad20cf77b287de670d5479a4
stasionok <stasionok@gmail.com>
2021-06-16 20:50:43 +0300

    Init latest 1.0.3 ver

---
+++ upload/catalog/controller/extension/module/scrfaq.php
@@ -0,0 +1,19 @@
+<?php
+
+class ControllerExtensionModuleScrfaq extends Controller
+{
+    public function index($setting)
+    {
+        $this->load->model('extension/module/scrfaq');
+        $this->load->language('extension/module/scrfaq');
+
+        $this->document->addScript('catalog/view/javascript/collapse.js');
+
+        $data['module_id'] = $setting['mid'];
+        $data['scrfaq_title'] = $setting['name'];
+        $data['scrfaq_descr'] = @$setting['descr'];
+        $data['answers'] = $this->model_extension_module_scrfaq->getAnswer($setting['mid']);
+
+        return $this->load->view('extension/module/scrfaq', $data);
+    }
+}
